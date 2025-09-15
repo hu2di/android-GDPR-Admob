202dece41fcf3a437aad6fe5c16197e3e258cf60
Huy-Hung Dinh <hebitaxy@gmail.com>
2025-09-15 23:19:27 +0700

    using ump lib from Google

--- app/src/main/java/io/github/hu2di/gdpradmob/FullscreenActivity.java
+++ app/src/main/java/io/github/hu2di/gdpradmob/FullscreenActivity.java
@@ -3,159 +3,114 @@ package io.github.hu2di.gdpradmob;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import com.google.ads.consent.ConsentForm;
-import com.google.ads.consent.ConsentFormListener;
-import com.google.ads.consent.ConsentInfoUpdateListener;
-import com.google.ads.consent.ConsentInformation;
-import com.google.ads.consent.ConsentStatus;
-import com.google.ads.consent.DebugGeography;
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.ump.ConsentDebugSettings;
+import com.google.android.ump.ConsentForm;
+import com.google.android.ump.ConsentInformation;
+import com.google.android.ump.ConsentRequestParameters;
+import com.google.android.ump.FormError;
+import com.google.android.ump.UserMessagingPlatform;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FullscreenActivity extends AppCompatActivity {
 
-    private View btnInterstitial;
-    private InterstitialAd mInterstitialAd;
-
+    private final String TAG = "hu2diMainActivity";
+    private View btnGdpr;
     private AdView smartBanner;
 
-    private LinearLayout adaptiveContainer;
-    private AdView adaptiveBanner;
+    private ConsentInformation consentInformation;
+    // Use an atomic boolean to initialize the Google Mobile Ads SDK and load ads once.
+    private final AtomicBoolean isMobileAdsInitializeCalled = new AtomicBoolean(false);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_fullscreen);
 
         initView();
-        initConsent();
-        initAdmob();
+
+        //GDPR
+        requestConsentForm();
     }
 
     private void initView() {
-        btnInterstitial = findViewById(R.id.btn_interstitial);
-        btnInterstitial.setOnClickListener(new View.OnClickListener() {
+        btnGdpr = findViewById(R.id.btn_gdpr);
+        btnGdpr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    Log.d("TAG", "The interstitial wasn't loaded yet.");
-                }
+                showConsentForm();
             }
         });
     }
 
-    private void initConsent() {
-        getConsent();
-       // dialogConsent();
-    }
-
-    private void getConsent() {
-        ConsentInformation consentInformation = ConsentInformation.getInstance(FullscreenActivity.this);
-
-        //Debug - Test - EU
-        consentInformation.addTestDevice("33BE2250B43518CCDA7DE426D04EE231");
-        consentInformation.setDebugGeography(DebugGeography.DEBUG_GEOGRAPHY_EEA);
-
-        String[] publisherIds = {"pub-0123456789012345"};
-        consentInformation.requestConsentInfoUpdate(publisherIds, new ConsentInfoUpdateListener() {
-            @Override
-            public void onConsentInfoUpdated(ConsentStatus consentStatus) {
-                // User's consent status successfully updated.
-
-                    dialogConsent();
-
-            }
-
-            @Override
-            public void onFailedToUpdateConsentInfo(String errorDescription) {
-                // User's consent status failed to update.
-                dialogConsent();
-            }
-        });
-    }
-
-    private void dialogConsent() {
-        URL privacyUrl = null;
-        try {
-            // TODO: Replace with your app's privacy policy URL.
-            privacyUrl = new URL("https://www.your.com/privacyurl");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            // Handle error.
-        }
-        ConsentForm form = new ConsentForm.Builder(FullscreenActivity.this, privacyUrl)
-                .withListener(new ConsentFormListener() {
-                    @Override
-                    public void onConsentFormLoaded() {
-                        // Consent form loaded successfully.
-                    }
-
-                    @Override
-                    public void onConsentFormOpened() {
-                        // Consent form was displayed.
-                    }
-
-                    @Override
-                    public void onConsentFormClosed(
-                            ConsentStatus consentStatus, Boolean userPrefersAdFree) {
-                        // Consent form was closed.
-                    }
-
-                    @Override
-                    public void onConsentFormError(String errorDescription) {
-                        // Consent form error.
-                    }
-                })
-                .withPersonalizedAdsOption()
-                .withNonPersonalizedAdsOption()
-                .withAdFreeOption()
+    private void requestConsentForm() {
+        // Debug only
+        ConsentDebugSettings debugSettings = new ConsentDebugSettings.Builder(this)
+                .setDebugGeography(ConsentDebugSettings.DebugGeography.DEBUG_GEOGRAPHY_EEA)
+                .addTestDeviceHashedId("") // find test device id in logcat
                 .build();
-        form.load();
-        form.show();
+
+        // Set tag for under age of consent. false mean users are not under age of consent.
+        ConsentRequestParameters params = new ConsentRequestParameters
+                .Builder()
+                .setConsentDebugSettings(debugSettings) // Debug only this param
+                .setTagForUnderAgeOfConsent(false)
+                .build();
+
+        consentInformation = UserMessagingPlatform.getConsentInformation(this);
+        consentInformation.reset(); // Debug only
+        consentInformation.requestConsentInfoUpdate(
+                this,
+                params,
+                (ConsentInformation.OnConsentInfoUpdateSuccessListener)  () -> {
+                    UserMessagingPlatform.loadAndShowConsentFormIfRequired(
+                            this,
+                            (ConsentForm.OnConsentFormDismissedListener) loadAndShowError -> {
+                                if (loadAndShowError != null) {
+                                    // Consent gathering failed.
+                                    Log.d(TAG, String.format("%s: %s",
+                                            loadAndShowError.getErrorCode(),
+                                            loadAndShowError.getMessage()));
+                                }
+
+                                // Consent has been gathered.
+                                if (consentInformation.canRequestAds()) {
+                                    initializeMobileAdsSdk();
+                                }
+                            }
+                    );
+                },
+                (ConsentInformation.OnConsentInfoUpdateFailureListener) requestConsentError -> {
+                    // Consent gathering failed.
+                    Log.d(TAG, String.format("%s: %s",
+                            requestConsentError.getErrorCode(),
+                            requestConsentError.getMessage()));
+                }
+        );
+
+        // Check if you can init Mobile Ads Sdk in parallel while checking for new consent info.
+        if (consentInformation.canRequestAds()) {
+            initializeMobileAdsSdk();
+        }
     }
 
-    private void initAdmob() {
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
+    private void initializeMobileAdsSdk() {
+        if (isMobileAdsInitializeCalled.getAndSet(true)) {
+            Log.d(TAG, "isMobileAdsInitializeCalled");
+            return;
+        }
+        Log.d(TAG, "isNOT MobileAdsInitializeCalled");
+
+        MobileAds.initialize(FullscreenActivity.this, initializationStatus -> {
+            initSmartBanner();
         });
-
-        initInterstitial();
-        initSmartBanner();
-        initAdaptiveBanner();
-    }
-
-    private void initInterstitial() {
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                // Load the next interstitial.
-                mInterstitialAd.loadAd(new AdRequest.Builder().build());
-            }
-
-        });
-        mInterstitialAd.loadAd(new AdRequest.Builder().build());
     }
 
     private void initSmartBanner() {
@@ -164,44 +119,15 @@ public class FullscreenActivity extends AppCompatActivity {
         smartBanner.loadAd(adRequest);
     }
 
-    private void initAdaptiveBanner() {
-        adaptiveContainer = findViewById(R.id.adaptiveContainer);
-        // Step 1 - Create an AdView and set the ad unit ID on it.
-        adaptiveBanner = new AdView(this);
-        adaptiveBanner.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
-        adaptiveContainer.addView(adaptiveBanner);
-        loadAdaptiveBanner();
-    }
-
-    private void loadAdaptiveBanner() {
-        // Create an ad request. Check your logcat output for the hashed device ID
-        // to get test ads on a physical device, e.g.,
-        // "Use AdRequest.Builder.addTestDevice("ABCDE0123") to get test ads on this
-        // device."
-        AdRequest adRequest =
-                new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                        .build();
-
-        AdSize adSize = getAdSize();
-        // Step 4 - Set the adaptive ad size on the ad view.
-        adaptiveBanner.setAdSize(adSize);
-
-        // Step 5 - Start loading the ad in the background.
-        adaptiveBanner.loadAd(adRequest);
-    }
-
-    private AdSize getAdSize() {
-        // Step 2 - Determine the screen width (less decorations) to use for the ad width.
-        Display display = getWindowManager().getDefaultDisplay();
-        DisplayMetrics outMetrics = new DisplayMetrics();
-        display.getMetrics(outMetrics);
-
-        float widthPixels = outMetrics.widthPixels;
-        float density = outMetrics.density;
-
-        int adWidth = (int) (widthPixels / density);
-
-        // Step 3 - Get adaptive ad size and return for setting on the ad view.
-        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
+    private void showConsentForm() {
+        UserMessagingPlatform.showPrivacyOptionsForm(FullscreenActivity.this,
+                new ConsentForm.OnConsentFormDismissedListener() {
+                    @Override
+                    public void onConsentFormDismissed(FormError formError) {
+                        if (formError != null) {
+                            Toast.makeText(FullscreenActivity.this, formError.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
